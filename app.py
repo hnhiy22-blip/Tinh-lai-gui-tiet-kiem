8bdb3fa6706ed221a8ea27853cb9feff2ea63e60
hnhiy22-blip <hnhiy22@gmail.com>
2026-06-16 14:01:46 +0700

    Update app title for savings calculator

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Tiêu đề ứng dụng
-st.title("💰 Ứng dụng tính tiền gửi tiết kiệm")
+st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM_ĐỀ TÀI 6_NGUYỄN HOÀI ")
 
 # Nhập dữ liệu
 C = st.number_input(
